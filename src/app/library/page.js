412603aa0a4569b4bbfd278bdb64b19c6969214d
Agneus <saminshararnafi@gmail.com>
2025-01-01 17:51:18 -0500

    remove library changes

--- src/app/library/page.js
+++ src/app/library/page.js
@@ -1,18 +1,14 @@
-"use client";
-
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 import Library from '@/components/Library';
-import { useLibraryFlashcards } from '@/contexts/LibraryFlashcardContext';
 
 export default async function LibraryPage() {
     const session = await getServerSession(authOptions);
-    const { libraryFlashcards } = useLibraryFlashcards();
 
     if (!session) {
         redirect('/api/auth/signin');
     }
 
-    return <Library key={libraryFlashcards}/>;
+    return <Library />;
 }
