fd4f3ae70d583e3a1d63403f2c1fc01e173c242a
Agneus <saminshararnafi@gmail.com>
2025-01-01 15:35:21 -0500

    library loading issue

--- src/app/library/page.js
+++ src/app/library/page.js
@@ -2,13 +2,15 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 import Library from '@/components/Library';
+import { useLibraryFlashcards } from '@/contexts/LibraryFlashcardContext';
 
 export default async function LibraryPage() {
     const session = await getServerSession(authOptions);
+    const { libraryFlashcards } = useLibraryFlashcards();
 
     if (!session) {
         redirect('/api/auth/signin');
     }
 
-    return <Library />;
+    return <Library key={libraryFlashcards}/>;
 }
